42a2e8f62c4f3438770d5ed74701be9aab73a0b5
ChrisKent <chrisgkent@gmail.com>
2026-09-01 14:40:15 +0100

    Reverted urls to production

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,9 +1,9 @@
 // The catalogue is served straight from the primer-schemes repository on GitHub.
 // Flipping to the production repo once its main branch carries the
 // v1.0.0-alpha index is a change to these three constants alone.
-export const CATALOG_REPO_OWNER = 'ChrisgKent'; // TODO: 'pha4ge' once main is migrated
-export const CATALOG_REPO_NAME = 'pha4ge-primer-schemes'; // TODO: 'primer-schemes' under pha4ge
-export const CATALOG_REPO_BRANCH = 'migration'; // TODO: 'main'
+export const CATALOG_REPO_OWNER = 'pha4ge'; // TODO: 'pha4ge' once main is migrated
+export const CATALOG_REPO_NAME = 'primer-schemes'; // TODO: 'primer-schemes' under pha4ge
+export const CATALOG_REPO_BRANCH = 'main'; // TODO: 'main'
 
 const RAW_BASE = `https://raw.githubusercontent.com/${CATALOG_REPO_OWNER}/${CATALOG_REPO_NAME}/${CATALOG_REPO_BRANCH}`;
 
